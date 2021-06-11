e2d849ac5b9906e828d5f52237ef8bdd698f50ac
Kevin Ziegler <ziegler.kevin@heb.com>
2021-06-11 13:52:46 -0500

    Add helpers for displaying dates

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-var fs = require("fs");
-var path = require('path');
-var Handlebars = require("handlebars");
+const fs = require("fs");
+const path = require('path');
+const Handlebars = require("handlebars");
 
-function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	var partialsDir = path.join(__dirname, 'partials');
-	var filenames = fs.readdirSync(partialsDir);
+const { dateHelpers } = require(join(__dirname, 'helpers/date.js'));
+const { MY, Y, DMY } = dateHelpers;
+
+const render = (resume) => {
+	const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	const tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	const partialsDir = path.join(__dirname, 'partials');
+	const filenames = fs.readdirSync(partialsDir);
 
 	filenames.forEach(function (filename) {
-	  var matches = /^([^.]+).hbs$/.exec(filename);
+	  const matches = /^([^.]+).hbs$/.exec(filename);
 	  if (!matches) {
 	    return;
 	  }
-	  var name = matches[1];
-	  var filepath = path.join(partialsDir, filename)
-	  var template = fs.readFileSync(filepath, 'utf8');
 
+	  const name = matches[1];
+	  const filepath = path.join(partialsDir, filename)
+	  const template = fs.readFileSync(filepath, 'utf8');
+
+    Handlebars.registerHelper('MY' MY);
+    Handlebars.registerHelper('Y' Y);
+    Handlebars.registerHelper('DMY' DMY);
 	  Handlebars.registerPartial(name, template);
 	});
+
 	return Handlebars.compile(tpl)({
 		css: css,
 		resume: resume
 	});
 }
 
-module.exports = {
-	render: render
-};
+module.exports = { render };
